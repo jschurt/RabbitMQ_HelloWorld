208feb47c14599e3482c7f6286a5436dd586ce13
jschurt <jschurt@test.com>
2019-05-01 18:27:48 -0400

    RabbitMQ - Hello World

---
+++ src/Consumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer
+{
+
+    //https://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                    //Cria uma nova queue se nao existir, ou abre a queue se existente
+                    channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: "hello",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+
+
+                } //using
+            } //using
+
+        } //Main
+    } //class
+} //namespace
---
+++ src/Publisher/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+
+    //https://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                    //Cria uma nova queue se nao existir, ou abre a queue se existente
+                    channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                } //using
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+
+            } //connection
+
+        } //main
+
+    } //class
+
+} //namespace
